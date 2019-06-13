910f23dbd37af01d62b72fe0f10a716270528089
nagataka1201 <49980667+nagataka1201@users.noreply.github.com>
2019-06-13 21:05:45 +0900

    Add files via upload

---
+++ image_recognition/AR/ViewController.swift
@@ -0,0 +1,92 @@
+//
+//  ViewController.swift
+//  AR
+//
+//  Created by 永野誉也 on 2019/06/03.
+//  Copyright © 2019 永野誉也. All rights reserved.
+//
+import UIKit
+import AVFoundation
+import Vision
+
+class ViewController: UIViewController {
+    @IBOutlet fileprivate weak var textView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // カメラキャプチャの開始
+        startCapture()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    // カメラキャプチャの開始
+    private func startCapture() {
+        let captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .photo
+        
+        // 入力の指定
+        guard let captureDevice = AVCaptureDevice.default(for: .video),
+            let input = try? AVCaptureDeviceInput(device: captureDevice),
+            captureSession.canAddInput(input) else {
+                assertionFailure("Error: 入力デバイスを追加できませんでした")
+                return
+        }
+        
+        captureSession.addInput(input)
+        
+        // 出力の指定
+        let output = AVCaptureVideoDataOutput()
+        output.setSampleBufferDelegate(self, queue: DispatchQueue(label: "VideoQueue"))
+        
+        guard captureSession.canAddOutput(output) else {
+            assertionFailure("Error: 出力デバイスを追加できませんでした")
+            return
+        }
+        captureSession.addOutput(output)
+        
+        // プレビューの指定
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.videoGravity = .resizeAspectFill
+        previewLayer.frame = view.bounds
+        view.layer.insertSublayer(previewLayer, at: 0)
+        
+        // キャプチャ開始
+        captureSession.startRunning()
+    }
+}
+
+extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        // CMSampleBufferをCVPixelBufferに変換
+        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+            assertionFailure("Error: バッファの変換に失敗しました")
+            return
+        }
+        
+        // CoreMLのモデルクラスの初期化
+        guard let model = try? VNCoreMLModel(for: Resnet50().model) else {
+            assertionFailure("Error: CoreMLモデルの初期化に失敗しました")
+            return
+        }
+        
+        // 画像認識リクエストを作成（引数はモデルとハンドラ）
+        let request = VNCoreMLRequest(model: model) { [weak self] (request: VNRequest, error: Error?) in
+            guard let results = request.results as? [VNClassificationObservation] else { return }
+            
+            // 判別結果とその確信度を上位5件まで表示
+            let displayText = results.prefix(5).compactMap { "\(Int($0.confidence * 100))% \($0.identifier.components(separatedBy: ", ")[0])" }.joined(separator: "\n")
+            
+            DispatchQueue.main.async {
+                self?.textView.text = displayText
+            }
+        }
+        
+        
+        // CVPixelBufferに対し、画像認識リクエストを実行
+        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
+    }
+}
